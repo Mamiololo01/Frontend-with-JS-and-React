dd9e20ac07c069498b91c2a13f4d157ca0ec999c
Christopher <jayces20@yahoo.com>
2021-12-07 17:04:57 +0100

    class constructor

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var day = {
-    breakfast: 'Eba with Egusi',
-    afternoon: 'Turkey with indomie'
-};
-
-var night = {
-    dinner: 'vegetables'
+function Holiday(destination, days) {
+    this.destination = destination
+    this.days = days
 }
 
-var reservation = {... day, ... night}
-console.log(reservation);
+Holiday.prototype.info = function() {
+    console.log(this.destination + '|' + this.days + 'days');
+};
+
+var Ali = new Holiday('Ali', '40')
+console.log(Ali.info());
